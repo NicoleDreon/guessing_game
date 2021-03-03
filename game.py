05cc2215799e2ffce895fe40d24f8a1ca893c38f
Nicole Dreon <dreon.nicole@gmail.com>
2021-03-03 01:23:21 +0000

    finished challenge

--- game.py
+++ game.py
@@ -1,5 +1,36 @@
 """A number-guessing game."""
+import random
+tries = 0
+x = random.randint(1, 100)
+# print(.format(x))
+# def random_number(x):
+    
+# from random import random_number
 
 print('Hello, player')
 name = input("What is your name?   ")
 print(f"Hello, {name}")
+# guess = input("Guess a number between 1 and 100?  ")
+
+while True:
+  guess = input("Your guess? ")
+  try: guess = int(guess)
+  except ValueError:
+    print(f"{guess} is not a valid number, try again.")
+    continue
+
+  tries += 1
+
+  if guess < 1 and guess > 100:
+    print("Not a valid entry")
+
+  if x > guess: 
+    print("Your guess is too low, try again")
+
+  elif x < guess:
+    print("Your guess is too high, try again")
+
+  else:
+    print(f"Well done, {name}!")
+    print(f"You found the number in {tries} tries!")
+    break
